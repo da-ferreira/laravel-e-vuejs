01e962fd71a002872b5a520a6da9e49b19617cf1
da-ferreira <ferreiradavid361@gmail.com>
2022-04-27 07:28:11 -0300

    Routes: fallback, nomeação, nomes e agrupamentos

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -30,24 +30,42 @@ Route::get(
 )->where('nome', '[A-Za-z ]+')->where('categoria_id', '[0-9]+');  # Regex
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
 Route::get('/login', function () {
     return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clentes');
+
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
 });
 
-Route::get('/clientes', function () {
-    return 'Clientes';
-});
 
-Route::get('/fornecedores', function () {
-    return 'Fornecedores';
-});
+Route::get('/rota1', function () {
+    echo 'Rota 1';
+})->name('site.rota1');
 
-Route::get('/produtos', function () {
-    return 'Produtos';
+
+Route::get('/rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('/rota2', 'rota1');
+
+Route::fallback(function () {
+    echo 'A rota acessada não existe. Clique <a href="' . route('site.index') . '">aqui para</a> ir para a página inicial';
 });
