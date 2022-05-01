6127f8a8d04fc0af105d24adf24cb1bab23118b5
da-ferreira <ferreiradavid361@gmail.com>
2022-05-01 18:49:30 -0300

    Configurando nome da tabela do BD associada a um Model

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    protected $table = 'fornecedores';  # Nome da tabela destino desse Modelo
+
+    # Autorizando, através do array associativo protected $fillable, que o método estático
+    # create receba através de um array os parametros do array
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
