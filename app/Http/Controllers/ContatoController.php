ebf115fe9e88fe5273aee829c09131c8269b85e5
David Ferreira <ferreiradavid361@gmail.com>
2022-05-02 15:11:25 -0300

    objeto request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
+        /*
+        # 1º forma de enviar:
+
+        $contato = new SiteContato();
+
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();
+        */
+
+        /*
+        # 2º forma de enviar:
+
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+        */
+
+        # 3º forma de enviar:
+        SiteContato::create($request->all());
+
         return view('site.contato');
     }
 }
