e786d3dc8ffe4efa371c9eddada26e79c172bbb2
da-ferreira <ferreiradavid361@gmail.com>
2022-05-08 19:04:15 -0300

    Middlewares (continuação)

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\LogAcesso;
 
 class LogAcessoMiddleware
 {
@@ -16,6 +17,11 @@ class LogAcessoMiddleware
     public function handle($request, Closure $next)
     {
         // return $next($request);
+
+        $ip = $request->server->get('REMOTE_ADDR');  // IP de quem acessou a rota
+        $rota = $request->getRequestUri();           // Rota acessada pelo IP
+
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
         return Response('Chegamos no middleware e finalizamos no próprio middleware');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,10 @@ Route::middleware(LogAcessoMiddleware::class)
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/contato', 'ContatoController@contato')
+    ->name('site.contato')
+    ->middleware(LogAcessoMiddleware::class);
+
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 
 Route::get('/login', function () {
